df6b6988815d6cfc9c7b974eb01ecf38d7388280
inouye165 <inouye165@gmail.com>
2025-07-22 16:01:51 -0700

    feat: Added a page for full view of individual van

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,41 +1,24 @@
 import React from "react"
-
-/**
- * {
-    * id: "1", 
-    * name: "Modest Explorer", 
-    * price: 60, 
-    * description: "The Modest Explorer is a van designed to get you out of the house and into nature. This beauty is equipped with solar panels, a composting toilet, a water tank and kitchenette. The idea is that you can pack up your home and escape for a weekend or even longer!", 
-    * imageUrl: "https://assets.scrimba.com/advanced-react/react-router/modest-explorer.png", 
-    * type: "simple"
- * }
- */
-
+import { Link } from "react-router-dom"
 
 export default function Vans() {
     const [vans, setVans] = React.useState([])
-
     React.useEffect(() => {
         fetch("/api/vans")
             .then(res => res.json())
-            .then(data => {
-                // You can add your console.log here
-                // It's good practice to return the data so the next .then can use it
-                console.log(JSON.stringify(data.vans, null, 2)); // Using JSON.stringify for better readability as you requested
-                return data;
-            })
             .then(data => setVans(data.vans))
     }, [])
 
-
     const vanElements = vans.map(van => (
         <div key={van.id} className="van-tile">
-            <img src={van.imageUrl} />
-            <div className="van-info">
-                <h3>{van.name}</h3>
-                <p>${van.price}<span>/day</span></p>
-            </div>
-            <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            <Link to={`/vans/${van.id}`}>
+                <img src={van.imageUrl} />
+                <div className="van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}<span>/day</span></p>
+                </div>
+                <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            </Link>
         </div>
     ))
 
