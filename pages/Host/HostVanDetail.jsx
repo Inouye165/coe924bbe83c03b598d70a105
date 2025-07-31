e51ffb0317c6c5b74a4a308598307754012b992a
inouye165 <inouye165@gmail.com>
2025-07-31 14:04:39 -0700

    feat: worked on search

--- pages/Host/HostVanDetail.jsx
+++ pages/Host/HostVanDetail.jsx
@@ -1,71 +1,49 @@
 import React from "react"
-import { useParams, Link, NavLink, Outlet } from "react-router-dom"
+import { Link, useParams, useLocation } from "react-router-dom"
 
-export default function HostVanDetail() {
-    const { id } = useParams()
-    const [currentVan, setCurrentVan] = React.useState(null)
-
-    const activeStyles = {
-        fontWeight: "bold",
-        textDecoration: "underline",
-        color: "#161616"
-    }
+export default function VanDetail() {
+    const params = useParams()
+    const location = useLocation()
+    console.log(location)
+    
+    const [van, setVan] = React.useState(null)
 
     React.useEffect(() => {
-        fetch(`/api/host/vans/${id}`)
+        fetch(`/api/vans/${params.id}`)
             .then(res => res.json())
-            .then(data => setCurrentVan(data.vans))
-    }, [])
+            .then(data => setVan(data.vans))
+    }, [params.id])
 
-    if (!currentVan) {
-        return <h1>Loading...</h1>
-    }
+    /**
+     * Challenge: modify the Link `to` prop below to send the user
+     * back to the previous page with the searchParams included, if
+     * they exist. (Remember we may not have anything in that state
+     * if there were no filters applied before coming to this
+     * van detail page, so make sure to "code defensively" to handle
+     * that case.)
+     */
+    const search = location.state?.search || ""
+    
     return (
-        <section>
+        <div className="van-detail-container">
             <Link
-                to=".."
+                to={`..${search}`}
                 relative="path"
                 className="back-button"
             >&larr; <span>Back to all vans</span></Link>
-
-            <div className="host-van-detail-layout-container">
-                <div className="host-van-detail">
-                    <img src={currentVan.imageUrl} />
-                    <div className="host-van-detail-info-text">
-                        <i
-                            className={`van-type van-type-${currentVan.type}`}
-                        >
-                            {currentVan.type}
-                        </i>
-                        <h3>{currentVan.name}</h3>
-                        <h4>${currentVan.price}/day</h4>
-                    </div>
+            
+            {van ? (
+                <div className="van-detail">
+                    <img src={van.imageUrl} />
+                    <i className={`van-type ${van.type} selected`}>
+                        {van.type}
+                    </i>
+                    <h2>{van.name}</h2>
+                    <p className="van-price"><span>${van.price}</span>/day</p>
+                    <p>{van.description}</p>
+                    <button className="link-button">Rent this van</button>
                 </div>
-
-                <nav className="host-van-detail-nav">
-                    <NavLink
-                        to="."
-                        end
-                        style={({ isActive }) => isActive ? activeStyles : null}
-                    >
-                        Details
-                    </NavLink>
-                    <NavLink
-                        to="pricing"
-                        style={({ isActive }) => isActive ? activeStyles : null}
-                    >
-                        Pricing
-                    </NavLink>
-                    <NavLink
-                        to="photos"
-                        style={({ isActive }) => isActive ? activeStyles : null}
-                    >
-                        Photos
-                    </NavLink>
-                </nav>
-                <Outlet context={{ currentVan }} />
-            </div>
-        </section>
+            ) : <h2>Loading...</h2>}
+        </div>
     )
 }
-
